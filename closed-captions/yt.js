749ccafe4dfb24aba2b4bdcf9e075faee0a67097
Martijn <mpkeesmaat@gmail.com>
2020-05-13 18:01:23 +0200

    did some things

--- closed-captions/yt.js
+++ closed-captions/yt.js
@@ -1,6 +1,7 @@
 var player;
 
 function onYouTubeIframeAPIReady() {
+<<<<<<< Updated upstream
 	player = new YT.Player('video-placeholder', {
 		width: 600,
 		height: 400,
@@ -20,6 +21,116 @@ function onYouTubeIframeAPIReady() {
 function initialize(){
 	// Update the controls on load
 	addSpans();
+=======
+  player = new YT.Player('video-placeholder', {
+    videoId: 'vrP-_T-h9YM',
+    playerVars: {
+      color: 'white',
+      rel: 0,
+      // autoplay: 1,
+      // playsinline: 0,
+      // showInfo: 0,
+      // controls: 0,
+      //start:
+      //playlist: 'taJ60kskkns,FG0fTKAqZ5g'
+    },
+    events: {
+      onReady: initialize,
+      onStateChange: broodjeAap,
+    },
+  });
+}
+
+const $bg = document.querySelector('.animated-bg');
+const $redSpotlight = document.querySelector('.red-spotlight');
+const $overlay = document.querySelector('.overlay');
+
+function broodjeAap(e) {
+  const $video = document.querySelector('#video-placeholder');
+  const $bgVid = document.querySelector('.bg-video');
+  console.log(e.data);
+  if (e.data == 1) {
+    $bgVid.src += '&autoplay=1';
+    $bgVid.src += '&mute=1';
+
+    $bg.classList.add('--is-active');
+    $redSpotlight.classList.add('--is-active');
+
+    // setTimeout(() => {
+    //   $video.style.transition = '8s all ease';
+    //   $video.style.transform = 'scaleY(.7)';
+    // }, 25000);
+
+    setTimeout(() => {
+      $video.style.display = 'none';
+    }, 33000);
+
+    setTimeout(() => {
+      $bgVid.classList.toggle('--is-active');
+    }, 39000);
+
+    // Return video
+    setTimeout(() => {
+      $bgVid.classList.toggle('--is-active');
+      $video.style.display = 'block';
+    }, 72000);
+
+    toggleOverlay(2, '#F2872D');
+    toggleOverlay(2.2, '#F2872D');
+
+    toggleOverlay(2.4, '#F2872D');
+    toggleOverlay(2.5, '#F2872D');
+
+    toggleOverlay(9, 'red', 'full');
+    toggleOverlay(11, 'red', 'full');
+
+    toggleOverlay(15.5, '#F2872D');
+    toggleOverlay(17, '#F2872D');
+
+    // toggleOverlay(23, 'red');
+    // toggleOverlay(24, 'red');
+
+    // $;
+
+    // toggleOverlay(22, 'red');
+    // toggleOverlay(14, 'red');
+  }
+}
+
+function toggleOverlay(time, color, full) {
+  setTimeout(() => {
+    $overlay.style.background = color;
+    $overlay.classList.toggle('--is-active');
+    full ? ($overlay.style.width = window.innerWidth + 'px') : ($overlay.style.width = '640px');
+    full ? ($overlay.style.height = window.innerHeight + 'px') : ($overlay.style.height = '360px');
+  }, time * 1000);
+}
+
+function initialize() {
+  // Get the last <li> element ("Milk") of <ul> with id="myList2"
+  var itm = document.getElementById('video-placeholder');
+
+  // Copy the <li> element and its child nodes
+  var cln = itm.cloneNode(true);
+  cln.classList.add('bg-video');
+
+  // Append the cloned <li> element to <ul> with id="myList1"
+  document.body.appendChild(cln);
+  // document.querySelector('.bg-video').src += '?start=3';
+
+  // Update the controls on load
+
+  // setTimeout(() => {
+  //   const trigger = document.querySelector('#player');
+  //   trigger.addEventListener('click', doIets);
+
+  //   function doIets() {
+  //     console.log('a');
+  //   }
+  // }, 2000);
+
+  addSpans();
+>>>>>>> Stashed changes
 }
 function addSpans(){
 	var ps = document.querySelectorAll('#closed-captions p');
